80897426308b564520365268479b9bc4ecc2dd96
silva.park <silva.park@sendbird.com>
2020-10-22 13:48:19 +0900

    added prac viewset codes
    - model viewset test
    - urlname and path matching check

--- prac/urls.py
+++ prac/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from prac.viewset import ReporterViewSet
+from prac.viewset import ReporterViewSet, ArticleModelViewSet
 
 app_name = "prac"
 
 router = DefaultRouter()
 router.register(r"reporters", ReporterViewSet, basename="reporter")
+router.register(r"articles", ArticleModelViewSet, basename="article")
 urlpatterns = [] + router.urls
--- prac/viewset.py
+++ prac/viewset.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.urls import reverse, reverse_lazy
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from prac.models import Reporter, Article
@@ -18,12 +20,14 @@ class ReporterViewSet(viewsets.ViewSet):
 
     def list(self, request):
         # GET /prac/reporters/
+        # urlname: "prac:reporters-list"
         queryset = Reporter.objects.all()
         serializer = ReporterSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         # GET /prac/reporters/1/
+        # urlname: "prac:reporters-detail"
         queryset = Reporter.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = ReporterSerializer(user)
@@ -31,19 +35,51 @@ class ReporterViewSet(viewsets.ViewSet):
 
     def create(self, request):
         # POST /prac/reporters/
+        # urlname: "prac:reporter-list"
         return Response("create")
 
     def update(self, request, pk=None):
-        # POST /prac/reporters/1/
+        # PUT /prac/reporters/1/
+        # urlname: "prac:reporter-detail"
         return Response("update")
 
     def partial_update(self, request, pk=None):
         # PATCH /prac/reporters/1/
+        # urlname: "prac:reporter-detail"
         return Response("partial_update")
 
     def destroy(self, request, pk=None):
         # DELETE /prac/reporters/1/
+        # urlname: "prac:reporter-detail"
         return Response("destroy")
+
+    @action(detail=False)
+    def hello(self, request):
+        # GET /prac/reporters/hello/
+        # urlname: "prac:reporter-hello"
+        return Response("hello")
+
+    @action(detail=True, methods=["post"])
+    def byebye(self, request, pk=None):
+        # GET /prac/reporters/1/byebye/
+        # urlname: "prac:reporter-byebye"
+        return Response(f"byebye: {pk}")
+
+    @action(detail=False)
+    def show_all_urlname(self, request):
+        # GET /prac/reporters/show-all-urlname/
+        # urlname: "prac:reporter-show_all_urlname"
+        return Response(
+            data={
+                "prac:reporter-list": reverse_lazy("prac:reporter-list"),
+                "prac:reporter-detail": reverse_lazy("prac:reporter-detail", args=[1]),
+                "prac:reporter-hello": reverse_lazy("prac:reporter-hello"),
+                "prac:reporter-byebye": reverse_lazy("prac:reporter-byebye", args=[1]),
+                "prac:reporter-show-all-urlname": reverse_lazy(
+                    "prac:reporter-show-all-urlname"
+                ),
+            }
+        )
 
 
 class ArticleModelViewSet(viewsets.ModelViewSet):
